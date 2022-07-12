946b0b09def93f11038a4302f76ba1a3db5ebcbc
Marcela Trujillo Toro <74881828+marcelaTrujilloToro@users.noreply.github.com>
2022-07-12 14:59:46 -0500

    Cleaning app

--- App.tsx
+++ App.tsx
@@ -1,25 +1,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import {DimensionesScreen} from './src/screens/DimensionesScreen';
-import {FlexScreen} from './src/screens/FlexScreen';
-import {PositionScreen} from './src/screens/PositionScreen';
-import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
-import {ContadorScreen} from './src/screens/ContadorScreen';
-import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
-import {TareaScreen} from './src/screens/TareaScreen';
+
+import { HolaMundo } from './src/screens/NavegacionApp/HolaMundo';
 
 const App = () => {
   return (
     //SafeAreaView: verifica si puede mostrar la informacion (que no choque el componente con el nodge)
     <SafeAreaView style={{flex: 1}}>
       {/* <HolaMundoScreen />
-      <ContadorScreen /> */}
-      {/* <BoxObjectModelScreen /> */}
-      {/* <DimensionesScreen/> */}
-      {/* <PositionScreen/> */}
-      {/* <FlexScreen/> */}
-      <TareaScreen />
+      <ContadorScreen />
+      <BoxObjectModelScreen />
+      <DimensionesScreen/>
+      <PositionScreen/>
+      <FlexScreen/>
+      <TareaScreen /> */}
+
+      <HolaMundo/>
     </SafeAreaView>
   );
 };
---
+++ src/screens/NavegacionApp/HolaMundo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const HolaMundo = () => {
+  return (
+    <View>
+      <Text>HolaMundo</Text>
+    </View>
+  );
+};
